f3f6840b3bd4e9d1a26bf0a85f267a9c4d698252
Maioluwu <168908657+Maioluwu@users.noreply.github.com>
2025-01-22 17:03:26 +0100

    Add files via upload

---
+++ portfolio/js/app.js
@@ -0,0 +1,39 @@
+const slide = document.querySelector('.text-center');
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight } = document.documentElement;
+
+    const elementTop = slide.getBoundingClientRect().top;
+    if (scrollTop > (scrollTop + elementTop).toFixed() - clientHeight * 0.50) {
+        slide.classList.add('active');
+    }
+});
+
+const menuHamburger = document.querySelector(".menu-burger");
+const navLinks = document.querySelector(".navlinks");
+
+menuHamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+});
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
+
+const footer = document.querySelector('footer');
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight } = document.documentElement;
+
+    const footerTop = footer.getBoundingClientRect().top;
+
+    if (scrollTop > (scrollTop + footerTop).toFixed() - clientHeight * 0.50) {
+        footer.classList.add('active');
+    }
+});
